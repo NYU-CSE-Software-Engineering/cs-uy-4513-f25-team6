a7a9367c2ebc8e672f8e0a7b01f5816928115af8
Robotgiggle <purplemrb12@gmail.com>
2025-11-17 19:16:20 -0500

    Set up logout button

--- spec/controllers/login_controller_spec.rb
+++ spec/controllers/login_controller_spec.rb
@@ -105,19 +105,19 @@ RSpec.describe LoginController, type: :controller do
     end
 
     
-   describe 'DELETE #logout' do
+   describe 'GET #logout' do
 
      it 'logs out the user' do
         # simulates a user being logged in by setting the user ID and role in the session
         session[:user_id] = 5 
         session[:role] = 'admin'
 
-        delete :logout
+        get :logout
 
         expect(session[:user_id]).to be_nil 
         expect(session[:role]).to be_nil 
         expect(response).to redirect_to(login_path)
-        expect(flash[:notice]).to eq('Logged out successfully')
+        expect(flash[:notice]).to eq('Successfully logged out')
      end 
 
    end
