3b905401fa515e9ce74941fed0251a5049cc69e4
Robotgiggle <purplemrb12@gmail.com>
2025-11-19 16:16:00 -0500

    Make the request spec pass

--- spec/requests/configure_time_slots_spec.rb
+++ spec/requests/configure_time_slots_spec.rb
@@ -2,16 +2,22 @@ require 'rails_helper'
 
 RSpec.describe "Configure Time Slots (request)", type: :request do
     let(:time_slot_class) { class_double("TimeSlot").as_stubbed_const }
+    let(:doctor_class) { class_double("Doctor").as_stubbed_const }
     
     describe "while logged in" do
         before :each do
             @doc = login_doctor
         end
 
+        def expect_configure_redirect
+            expect(response).to redirect_to(configure_time_slots_path)
+            expect(doctor_class).to receive(:find).and_return(@doc)
+            expect(@doc).to receive(:time_slots).and_return([])
+            follow_redirect!
+        end
+
         describe "GET /doctor/time_slots" do
             it "shows your current slots when logged in as doctor" do
-                doc = login_doctor
-    
                 slot1 = instance_double("TimeSlot", id: 101,
                                         starts_at: Time.zone.parse("2000-01-01 09:00"),
                                         ends_at:   Time.zone.parse("2000-01-01 09:30"))
@@ -19,7 +25,8 @@ RSpec.describe "Configure Time Slots (request)", type: :request do
                                         starts_at: Time.zone.parse("2000-01-01 09:30"),
                                         ends_at:   Time.zone.parse("2000-01-01 10:00")) 
     
-                expect(time_slot_class).to receive(:where).with(@doc.id).and_return([slot1, slot2])
+                expect(doctor_class).to receive(:find).with(@doc.id).and_return(@doc)
+                expect(@doc).to receive(:time_slots).and_return([slot1, slot2])
     
                 get configure_time_slots_path
     
@@ -32,12 +39,13 @@ RSpec.describe "Configure Time Slots (request)", type: :request do
         describe "POST /doctors/:id/time_slots" do
             it "attempts to create a new time slot" do
                 new_slot = instance_double("TimeSlot")
-                expect(time_slot_class).to receive(:new).with(@doc.id, "3:00 PM", "3:15 PM").and_return(new_slot)
+                expect(time_slot_class).to receive(:new).with({doctor_id: @doc.id.to_s, starts_at: "3:00 PM", ends_at: "3:15 PM"}).and_return(new_slot)
                 expect(new_slot).to receive(:valid?).and_return(true)
                 expect(new_slot).to receive(:save)
     
                 post doctor_time_slots_path(@doc.id), params: {starts_at: "3:00 PM", ends_at: "3:15 PM"}
     
+                expect_configure_redirect
                 expect(response.body).to include("Succesfully added new time slot")
             end
     
@@ -46,10 +54,12 @@ RSpec.describe "Configure Time Slots (request)", type: :request do
                 expect(time_slot_class).to receive(:new).and_return(bad_slot)
                 expect(bad_slot).to receive(:valid?).and_return(false)
                 expect(bad_slot).not_to receive(:save)
+                expect(bad_slot).to receive_message_chain(:errors, :as_json).and_return("{}")
     
                 post doctor_time_slots_path(@doc.id), params: {starts_at: "3:00 PM", ends_at: "2:00 PM"}
     
-                expect(response.body).to include("Invalid time slot details")
+                expect_configure_redirect
+                expect(response.body).to include("Invalid time slot details: ")
             end
 
             it "fails if trying to add a slot to someone else" do
@@ -57,40 +67,43 @@ RSpec.describe "Configure Time Slots (request)", type: :request do
     
                 post doctor_time_slots_path(@doc.id + 1), params: {starts_at: "3:00 PM", ends_at: "3:15 PM"}
     
-                expect(response.body).to include("Can't add a slot to someone else (if you're seeing this, something went wrong)")
+                expect_configure_redirect
+                expect(response.body).to include("Attempted to add a time slot to another doctor (this should not be possible)")
             end
         end
     
         describe "DELETE /doctors/:id/time_slots/:tid" do
             it "attempts to delete the specified time slot" do
                 to_delete = instance_double("TimeSlot")
-                expect(time_slot_class).to receive(:find_by).with(14).and_return(to_delete)
+                expect(time_slot_class).to receive(:find_by).with({id: "14"}).and_return(to_delete)
                 expect(to_delete).to receive_message_chain(:doctor, :id).and_return(@doc.id)
-                expect(time_slot_class).to receive(:destroy).with(14)
+                expect(to_delete).to receive(:destroy)
 
-                delete delete_doctor_time_slot_path(@doc.id, 14)
+                delete doctor_time_slot_path(@doc.id, 14)
 
+                expect_configure_redirect
                 expect(response.body).to include("Successfully removed time slot")
             end
     
             it "fails if the slot is owned by someone else" do
                 to_delete = instance_double("TimeSlot")
-                expect(time_slot_class).to receive(:find_by).with(14).and_return(to_delete)
+                expect(time_slot_class).to receive(:find_by).with({id: "14"}).and_return(to_delete)
                 expect(to_delete).to receive_message_chain(:doctor, :id).and_return(@doc.id + 1)
-                expect(time_slot_class).not_to receive(:destroy)
+                expect(to_delete).not_to receive(:destroy)
 
-                delete delete_doctor_time_slot_path(@doc.id, 14)
+                delete doctor_time_slot_path(@doc.id, 14)
 
-                expect(response.body).to include("Can't remove someone else's slot (if you're seeing this, something went wrong)")
+                expect_configure_redirect
+                expect(response.body).to include("Attempted to remove a slot from another doctor (this should not be possible)")
             end
 
             it "fails if the slot doesn't exist" do
-                expect(time_slot_class).to receive(:find_by).with(14).and_return(nil)
-                expect(time_slot_class).not_to receive(:destroy)
+                expect(time_slot_class).to receive(:find_by).with({id: "14"}).and_return(nil)
 
-                delete delete_doctor_time_slot_path(@doc.id, 14)
+                delete doctor_time_slot_path(@doc.id, 14)
 
-                expect(response.body).to include("Can't remove a slot that doesn't exist (if you're seeing this, something went wrong)")
+                expect_configure_redirect
+                expect(response.body).to include("Attempted to remove a nonexistent slot (this should not be possible)")
             end
         end
     end
@@ -101,7 +114,7 @@ RSpec.describe "Configure Time Slots (request)", type: :request do
 
             expect(response).to redirect_to(login_path)
             follow_redirect!
-            expect(response.body).to include("This page requires you to be logged in")
+            expect(response.body).to include("This page or action requires you to be logged in")
         end
 
         it "POST /doctors/:id/time_slots kicks you out" do
@@ -109,15 +122,15 @@ RSpec.describe "Configure Time Slots (request)", type: :request do
 
             expect(response).to redirect_to(login_path)
             follow_redirect!
-            expect(response.body).to include("This action requires you to be logged in")
+            expect(response.body).to include("This page or action requires you to be logged in")
         end
 
         it "DELETE /doctors/:id/time_slots/:tid kicks you out" do
-            delete delete_doctor_time_slot_path(7, 14)
+            delete doctor_time_slot_path(7, 14)
 
             expect(response).to redirect_to(login_path)
             follow_redirect!
-            expect(response.body).to include("This action requires you to be logged in")
+            expect(response.body).to include("This page or action requires you to be logged in")
         end
     end
 end
