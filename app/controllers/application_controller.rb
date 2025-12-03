a036c31f6d49067329c1b7dcba54c47f2a7236a3
Robotgiggle <purplemrb12@gmail.com>
2025-12-03 10:59:36 -0500

    Add sign-up links (and login status) to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,17 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  before_action :get_user
+
   private
+
+  def get_user()
+    case session[:role]
+    when 'patient' then @user = Patient.find(session[:user_id])
+    when 'doctor' then @user = Doctor.find(session[:user_id])
+    when 'admin' then @user = Admin.find(session[:user_id])
+    end
+  end
 
   def check_login(roles)
     if (!session[:user_id] || !roles.include?(session[:role]))
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,10 @@
 class DashboardController < ApplicationController
     def patient
-        @user = Patient.find(session[:user_id])
     end
     
     def doctor
-        @user = Doctor.find(session[:user_id])
     end
 
     def admin
-        @user = Admin.find(session[:user_id])
     end
 end
