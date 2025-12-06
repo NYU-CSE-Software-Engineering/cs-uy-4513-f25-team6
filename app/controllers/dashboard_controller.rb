a6c5858512ccc8b91835372e7518f25e2ae663fd
Ananya Shah <ananyashah@Ananyas-MacBook-Air.local>
2025-12-05 20:51:05 -0500

    Implement basic patient, docto and admin dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,30 @@
 class DashboardController < ApplicationController
-    def patient
-    end
-    
-    def doctor
-    end
+  def patient
 
-    def admin
-    end
+    @appointments = Appointment
+                      .joins(:time_slot)
+                      .includes(:time_slot, :doctor)
+                      .where(patient_id: @user.id)
+                      .order(:date, 'time_slots.starts_at')
+
+    @unpaid_bills = Bill
+                      .joins(:appointment)
+                      .where(appointments: { patient_id: @user.id }, status: "unpaid")
+  end
+
+  def doctor
+
+    @appointments = Appointment
+                      .joins(:time_slot)
+                      .includes(:patient, :time_slot)
+                      .where(time_slots: { doctor_id: @user.id })
+                      .order(:date, 'time_slots.starts_at')
+  end
+
+  def admin
+    @patients_count     = Patient.count
+    @doctors_count      = Doctor.count
+    @appointments_count = Appointment.count
+    @unpaid_bills_count = Bill.where(status: "unpaid").count
+  end
 end
