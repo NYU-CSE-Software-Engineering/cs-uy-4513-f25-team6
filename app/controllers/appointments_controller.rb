9b9a5c3493dc4dd292edf7ad77d4c0e779119706
NELSON_JIANG <jiangnelson999@gmail.com>
2025-12-07 22:41:34 -0500

    changed doctor index to link to time time slots

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,7 +4,7 @@ class AppointmentsController < ApplicationController
     slot = TimeSlot.find(params.dig(:appointment, :time_slot_id))
     date = params.dig(:appointment, :date)
 
-    if Appointment.exists?(time_slot: slot)
+    if Appointment.exists?(time_slot: slot, date: date)
       redirect_to doctor_time_slots_path(slot.doctor.id), alert: "Time slot no longer available"
       return
     end
